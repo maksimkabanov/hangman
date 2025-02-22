4fd229045ad7c90d0f209049aa0e6597e907e6c7
maksimkabanov <maksimkabanov@gmail.com>
2025-02-22 19:35:56 +0100

    mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
-      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
-        Click me
-      </button>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
